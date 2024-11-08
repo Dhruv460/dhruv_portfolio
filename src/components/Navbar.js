755074ecdd5b6be93c64a3fc3e6d741ed7f0ac61
Dhruv <mradhruv460@gmail.com>
2024-11-09 02:07:39 +0530

    changed the logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/Dhruv.png";
+import logo from "../Assets/heart.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -39,8 +39,18 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <img
+            src={logo}
+            className="logo_dhruv"
+            alt="brand"
+            style={{
+              height: "45px",
+              width: "auto",
+              mixBlendMode: "color-burn",
+            }}
+          />
         </Navbar.Brand>
+
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
